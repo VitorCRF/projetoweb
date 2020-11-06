0fd609306afcf708e13272dacd795b62b3a26b25
Vitor Félix de Araujo <vitorfelix37@gmail.com>
2020-11-06 16:04:59 -0300

    menu responsivo

--- js/funcoes_index.js
+++ js/funcoes_index.js
@@ -29,4 +29,38 @@ $(document).ready(function(){
         atualizarInformacoes();
     });
 
+    $("#label").click(function(){
+        $('ul').toggleClass('show')
+    });
+    $("#label").click(function(){
+        $('#carousel').toggleClass('esconde')
+    });
+    
+    $("#produtos").click(function(){
+        $('#produtos').addClass('active')
+        $('#home').removeClass('active')
+        $('#depoimentos').removeClass('active')
+        $('#servicos').removeClass('active')
+    });
+    $("#home").click(function(){
+        $('#home').addClass('active')
+        $('#produtos').removeClass('active')
+        $('#depoimentos').removeClass('active')
+        $('#servicos').removeClass('active')
+    });
+    $("#depoimentos").click(function(){
+        $('#depoimentos').addClass('active')
+        $('#produtos').removeClass('active')
+        $('#home').removeClass('active')
+        $('#servicos').removeClass('active')
+    });
+    $("#servicos").click(function(){
+        $('#servicos').addClass('active')
+        $('#produtos').removeClass('active')
+        $('#home').removeClass('active')
+        $('#depoimentos').removeClass('active')
+    });
+    
+    
+
 });
