6f501e29532ae0d0060d3fa56be8070dec7cd6f1
Vitor Félix de Araujo <vitorfelix37@gmail.com>
2020-11-07 16:56:48 -0300

    novas paginas

---
+++ js/carrinho.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+    $("#label").click(function(){
+        $('ul').toggleClass('show')
+    });
+    $("#label").click(function(){
+        $('#carousel').toggleClass('esconde')
+    });
+
+
+
+
+    $("#carrinho").click(function(){
+        
+    });
+
+
+    $("#produtos").click(function(){
+        $('#produtos').addClass('active')
+        $('#home').removeClass('active')
+        $('#depoimentos').removeClass('active')
+        $('#servicos').removeClass('active')
+    });
+    $("#home").click(function(){
+        $('#home').addClass('active')
+        $('#produtos').removeClass('active')
+        $('#depoimentos').removeClass('active')
+        $('#servicos').removeClass('active')
+    });
+    $("#depoimentos").click(function(){
+        $('#depoimentos').addClass('active')
+        $('#produtos').removeClass('active')
+        $('#home').removeClass('active')
+        $('#servicos').removeClass('active')
+    });
+    $("#servicos").click(function(){
+        $('#servicos').addClass('active')
+        $('#produtos').removeClass('active')
+        $('#home').removeClass('active')
+        $('#depoimentos').removeClass('active')
+    });
+    
+    
+
+});
