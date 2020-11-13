f4703eb282e4c751f649382417d0d8eec6d6126c
Vitor Félix de Araujo <vitorfelix37@gmail.com>
2020-11-13 12:52:57 -0300

    finalizacao produtos

--- js/produtos.js
+++ js/produtos.js
@@ -1,5 +1,5 @@
 var array_Templates =   [["Educacional ","../img/template-educacional.jpg", false],
-                        ["Cake Site", "../img/cake-site-template-img.png", true],
+                        ["Cake Site", "../img/cake-site-template-img.png", false],
                         ["Investimentos", "../img/template-empresa-de-investimentos.jpg", false],
                         ["Sistema Completo", "../img/template-sistema-completo.png", false],
                         ["Tecnologia", "../img/template-tecnologia.jpg", false],
@@ -19,8 +19,6 @@ $(document).ready(function(){
         $('.best-seller').toggleClass('esconde')
     });
     
-
-    
     listaProdutos();
 
 });
@@ -28,24 +26,36 @@ $(document).ready(function(){
 
 function listaProdutos(){
 
-    
+    $(".div-best-sellers").html("");
+
     for (var i = 0; i < array_Templates.length; i++){
         var conteudo = "";
             conteudo += '<div class="best-seller">';
             conteudo +=      '<h1 class="titulo-best-seller" align="center">'+array_Templates[i][0]+'</h1>';
-                conteudo +=      '<img width="300px" height="200px" alt="'+array_Templates[i][0]+'" src="'+array_Templates[i][1]+'"class="imagem-best-seller">';
+            conteudo +=      '<img width="300px" height="200px" alt="'+array_Templates[i][0]+'" src="'+array_Templates[i][1]+'"class="imagem-best-seller">';
             if(array_Templates[i][2] == false){
                 conteudo +=      '<button class="bAdicionarCarrinho" id_produto="'+i+'">Adicionar ao carrinho</button>';
             }
             else{
-                conteudo +=      '<button class="bAdicionarCarrinho bAdicionado" id_produto="'+i+'">Adicionado ao carrinho</button>';
+                conteudo +=      '<button class="bAdicionado" id_produto="'+i+'">Adicionado ao carrinho</button>';
+                
             }
             conteudo += '</div>';
         $(".div-best-sellers").append(conteudo);
         
     }
     $(".bAdicionarCarrinho").click(function(){
-        window.alert(oi);
+        
+
+        var id = $(this).attr("id_produto");
+        
+        array_Templates[id].splice(2, 1, true);
+
+        carrinho.push(array_Templates[id]);
+        console.log(carrinho);
+
+        listaProdutos();
+
     });
     
     
