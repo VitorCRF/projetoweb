d0e8c3b6cb67ce864c2c9cfa46fa0a237a278176
tiagofelipemuller <tiagofelipemuller@gmail.com>
2020-11-14 14:48:30 -0300

    Correção do problema de troca de pagina ao apertar o botão Cadastre-se

--- js/login.js
+++ js/login.js
@@ -1,45 +1,39 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  $("#label").click(function () {
+    $("ul").toggleClass("show");
+  });
+  $("#label").click(function () {
+    $("#carousel").toggleClass("esconde");
+  });
 
-    $("#label").click(function(){
-        $('ul').toggleClass('show')
-    });
-    $("#label").click(function(){
-        $('#carousel').toggleClass('esconde')
-    });
+  $("#carrinho").click(function () {});
 
+  $("#produtos").click(function () {
+    $("#produtos").addClass("active");
+    $("#home").removeClass("active");
+    $("#depoimentos").removeClass("active");
+    $("#servicos").removeClass("active");
+  });
+  $("#home").click(function () {
+    $("#home").addClass("active");
+    $("#produtos").removeClass("active");
+    $("#depoimentos").removeClass("active");
+    $("#servicos").removeClass("active");
+  });
+  $("#depoimentos").click(function () {
+    $("#depoimentos").addClass("active");
+    $("#produtos").removeClass("active");
+    $("#home").removeClass("active");
+    $("#servicos").removeClass("active");
+  });
+  $("#servicos").click(function () {
+    $("#servicos").addClass("active");
+    $("#produtos").removeClass("active");
+    $("#home").removeClass("active");
+    $("#depoimentos").removeClass("active");
+  });
 
-
-
-    $("#carrinho").click(function(){
-        
-    });
-
-
-    $("#produtos").click(function(){
-        $('#produtos').addClass('active')
-        $('#home').removeClass('active')
-        $('#depoimentos').removeClass('active')
-        $('#servicos').removeClass('active')
-    });
-    $("#home").click(function(){
-        $('#home').addClass('active')
-        $('#produtos').removeClass('active')
-        $('#depoimentos').removeClass('active')
-        $('#servicos').removeClass('active')
-    });
-    $("#depoimentos").click(function(){
-        $('#depoimentos').addClass('active')
-        $('#produtos').removeClass('active')
-        $('#home').removeClass('active')
-        $('#servicos').removeClass('active')
-    });
-    $("#servicos").click(function(){
-        $('#servicos').addClass('active')
-        $('#produtos').removeClass('active')
-        $('#home').removeClass('active')
-        $('#depoimentos').removeClass('active')
-    });
-    
-    
-
+  $("#botao-cadastrar").click(function () {
+    window.location.href = "cadastro.html";
+  });
 });
